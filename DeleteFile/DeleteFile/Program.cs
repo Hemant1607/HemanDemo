67327a6f54c2935549c2caee0adcfa721dc116b8
Hemant1607 <hemant1607.hk@gmail.com>
2021-09-15 16:15:37 +0530

    15/09/2021 question2

---
+++ DeleteFile/DeleteFile/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DeleteFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter file to be deleted");
+            string routefolder = @"C:\training\Eurotraining\FileHandling";
+            string filename = Console.ReadLine();
+            string file = Path.Combine(routefolder, filename);
+            Console.WriteLine(file);
+            if (File.Exists(file))
+            {
+                File.Delete(file);
+                Console.WriteLine("File deleted successfully");
+                FileStream fs = new FileStream(file, FileMode.OpenOrCreate, FileAccess.Write);
+                fs.Close();
+            }
+            else
+            {
+                Console.WriteLine("File does not exist");
+                FileStream fs = new FileStream(file, FileMode.OpenOrCreate, FileAccess.Write);
+                fs.Close();
+            }
+            Console.Read();
+            
+        }
+    }
+}
