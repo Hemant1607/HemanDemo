c8643d5c935b03714a099db5f8a9af8acce6bd69
Hemant1607 <hemant1607.hk@gmail.com>
2021-09-15 16:17:12 +0530

    15/09/2021 Question3

---
+++ CreatEditor/CreatEditor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace CreatEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (File.Exists(@"C:\training\Eurotraining\FileHandling\mytest.txt"))
+            {
+                FileStream fr = new FileStream(@"C:\training\Eurotraining\FileHandling\mytest.txt", FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(fr);
+                while (sr.Peek() > 0)
+                {
+                    Console.WriteLine(sr.ReadLine());
+                }
+            }
+            else
+            {
+                FileStream fs = new FileStream(@"C:\training\Eurotraining\FileHandling\mytest.txt", FileMode.OpenOrCreate, FileAccess.Write);
+                Console.WriteLine("A file created with name mytest.txt");
+            }
+            Console.Read();
+        }
+    }
+}
