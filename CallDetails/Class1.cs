0a6d8b04fbb84d7e0d91bab623316f3c732fd119
Hemant1607 <hemant1607.hk@gmail.com>
2021-09-15 14:05:53 +0530

    Uploaded call details program

---
+++ CallDetails/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CallDetails
+{
+    class Class1
+    {
+        public int ReadFile()
+        {
+            FileStream fs = new FileStream(@"C:\training\Eurotraining\FileHandling\temp2.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string[] j;
+            while (sr.Peek()> 0)
+            {
+                
+                for (int i = 0; i < 4; i++)
+                {
+                    string line = sr.ReadLine();
+                    j = line.Split(':');
+                    if (i == 2)
+                    {
+                        string[] k = j[1].Split(' ');
+                        Console.Write(k[0]);
+                        Console.Write("\t");
+                        continue;
+                    }      
+                    Console.Write(j[1]);
+                    Console.Write("\t");
+                    
+                }
+                
+                Console.WriteLine();
+                string line1 = sr.ReadLine();
+            }
+        
+            return 20;
+        }
+    }
+}
---
+++ CallDetails/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallDetails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Source" + "\t\t" + "Destination" + "\t" + "Date" + "\t\t" + "Call Status");
+            Class1 objFile = new Class1();
+            objFile.ReadFile();
+            Console.Read();
+        }
+    }
+}
