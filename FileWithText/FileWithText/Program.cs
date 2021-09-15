c93decf2d5d0bfe510f0f9143986416afcbae777
Hemant1607 <hemant1607.hk@gmail.com>
2021-09-15 16:18:27 +0530

    15/09/2021 Question4

---
+++ FileWithText/FileWithText/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace FileWithText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream(@"C:\training\Eurotraining\FileHandling\mytest.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter ObjWrite = new StreamWriter(fs);
+            ObjWrite.WriteLine("Hello and Welcome");
+            ObjWrite.WriteLine("It is the first content");
+            ObjWrite.WriteLine("of the text file mytest.txt");
+            ObjWrite.Close();
+            fs.Close();
+            Console.WriteLine("A file created with content name mytest.txt");
+            Console.Read();
+        }
+    }
+}
