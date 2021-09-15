69ffa416b33aea968d39773ccb621411f7f6fbe5
Hemant1607 <hemant1607.hk@gmail.com>
2021-09-15 14:09:04 +0530

    Uploaded second program

---
+++ Program2/FileHandling.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MyFirstConsoleApp
+{
+    class FileHandling
+    {
+        public string WriteFile()
+        {
+            int value;
+            FileStream fs = new FileStream(@"C:\training\Eurotraining\FileHandling\temp.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter ObjWrite = new StreamWriter(fs);
+            value = Convert.ToInt32(Console.ReadLine());
+            ObjWrite.WriteLine("Course Details");
+            ObjWrite.WriteLine("***************************");
+            ObjWrite.WriteLine("Number of courses: "+value);
+            ObjWrite.WriteLine();
+
+            for (int i = 0; i < value; i++)
+            {
+                Console.Write("Enter registration number: ");
+                int reg = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter Course: ");
+                string course = Console.ReadLine();
+                Console.Write("Enter Course Title: ");
+                string coursetitle = Console.ReadLine();
+                Console.WriteLine();
+                ObjWrite.WriteLine("Registration number :" + reg);
+                ObjWrite.WriteLine("Course :" + course);
+                ObjWrite.WriteLine("Title :" + coursetitle);
+                ObjWrite.WriteLine();
+            }
+            ObjWrite.Close();
+            fs.Close();
+            return "Welcome";
+        }
+        public int ReadFile()
+        {
+            FileStream fs = new FileStream(@"C:\training\Eurotraining\FileHandling\temp.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            Console.WriteLine(sr.ReadLine());
+            Console.WriteLine(sr.ReadLine());
+            Console.WriteLine(sr.ReadLine());
+            Console.WriteLine(sr.ReadLine());
+            Console.WriteLine(sr.ReadLine());
+           
+            return 20;
+        }
+    }
+}
---
+++ Program2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+            Console.Write("Enter number of values: ");
+            FileHandling objFile = new FileHandling();
+            objFile.WriteFile();
+            Console.Read();
+            //string value2 = objFile.ReadFile().ToString();
+            //Console.WriteLine(value1);
+            //Console.WriteLine(value2);
+            //Console.Read();
+            //int value1; int value2; int value3, value4, value5; float value6;
+            //Console.Write("Enter value1: ");
+            //value1 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Enter value2: ");
+            //value2 = Convert.ToInt32(Console.ReadLine());
+            //value3 = value1 + value2;
+            //value4 = value2 - value1;
+            //value5 = value1 * value2;
+            //value6 = value2 / value1;
+            //Console.WriteLine("Addition: " + value1 * value2);
+            //Console.WriteLine("Subtraction: " + value4);
+            //Console.WriteLine("Multiplication: " + value5);
+            //Console.WriteLine("Division: " + value6);
+            //Console.Read();
+
+        }
+    }
+}
