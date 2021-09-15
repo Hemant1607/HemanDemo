4e1963878fadb6fc8a53dd5fd36e4e65742ab574
Hemant1607 <hemant1607.hk@gmail.com>
2021-09-15 16:19:17 +0530

    15/09/2021 Question5

---
+++ FileWriteRead/FileWriteRead/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileWriteRead
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream(@"C:\training\Eurotraining\FileHandling\mytest.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter ObjWrite = new StreamWriter(fs);
+            ObjWrite.WriteLine("Hello and Welcome");
+            ObjWrite.WriteLine("It is the first content");
+            ObjWrite.WriteLine("of the text file mytest.txt");
+
+            ObjWrite.Close();
+            fs.Close();
+
+            Console.WriteLine("Here is the contet of the file mytest.txt:");
+
+            FileStream fr = new FileStream(@"C:\training\Eurotraining\FileHandling\mytest.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fr);
+            while (sr.Peek() > 0)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+            sr.Close();
+            fr.Close();
+            Console.Read();
+        }
+    }
+}
