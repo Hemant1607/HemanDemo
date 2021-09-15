cf2be0461084344e49883436548b17c8fa5d9a24
Hemant1607 <hemant1607.hk@gmail.com>
2021-09-15 16:21:20 +0530

    15/09/2021 Question7

---
+++ NumLines/NumLines/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace NumLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream(@"C:\training\Eurotraining\FileHandling\mytest.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            Console.WriteLine("Here are the contents of mytest.txt:");
+            int j = 0;
+            while (sr.Peek() > 0)
+            {
+                Console.WriteLine(sr.ReadLine());
+                j++;
+            }
+            Console.WriteLine("Number of lines in mytest.txt is: " + j);
+            Console.Read();
+        }
+    }
+}
