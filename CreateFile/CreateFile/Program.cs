e8cf11884637399247ba49af6eafc63906b871b2
Hemant1607 <hemant1607.hk@gmail.com>
2021-09-15 16:02:07 +0530

    15/09/2021 1st question

---
+++ CreateFile/CreateFile/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace CreateFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream(@"C:\training\Eurotraining\FileHandling\mytest.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            Console.WriteLine("A file created with name mytest.txt");
+            Console.Read();
+            fs.Close();
+        }
+    }
+}
